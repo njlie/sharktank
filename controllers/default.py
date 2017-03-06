28edbe33a746801e33c569e2f044ac466108699e
mjschmid <mjschmid@ucsc.edu>
2017-03-05 16:34:35 -0800

    ideasList changes

--- controllers/default.py
+++ controllers/default.py
@@ -69,13 +69,17 @@ def logedIn():
 
 # /////////////////////////////////////////////////////////////////////////
 def showIdea():
-    post = db.idea(request.args(0, cast=int))
-    return dict(post=post)
+    post = db.idea(request.args(0, cast=int))   # this is the idea
+    db.comm.idea_id.default = post.id       # set the idea id of the comments to the post id
+    form = SQLFORM(db.comm)     # this is the form for filling out a comment
+    if form.process().accepted:     # if the comment is valid
+        response.flash = 'your comment is posted'
+    comments = db(db.comm.idea_id == post.id).select()  # the comments that are associated with that idea
+    return dict(post=post, form=form, comments=comments)
 
 #///////////////////////////////////////////////////////////////////////////
 def ideasList():
-    rows = db(db.idea).select()
-    #joinRows = db(db.idea.id == db.idea_group.id).select() # tryed to do join to give user option to edit
+    rows = db(db.idea).select()     # select the ideas
     return dict(rows=rows)
 
 #///////////////////////////////////////////////////////////////////////////
